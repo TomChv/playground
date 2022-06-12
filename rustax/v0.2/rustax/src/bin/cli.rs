de19a2d24b333cfdf094f14a8b275017d3d4ace5
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-06-12 22:46:23 +0200

    feat(rustax/v0.2.0): implement cli binary
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- rustax/v0.2/rustax/src/bin/cli.rs
+++ rustax/v0.2/rustax/src/bin/cli.rs
@@ -1,5 +1,14 @@
-use tax;
+use clap::Parser;
+
+use cli::Args;
+use tax::Calculator;
 
 fn main() {
-    println!("Welcome to rustax CLI");
+    let args = Args::parse();
+
+    let amount = args.amount;
+    let calc = Calculator::new(amount);
+    let result = calc.taxe();
+
+    println!("For an income of {}, you'll pay {}€ of tax", amount, result);
 }
