c12c1a36152976bc193e7805a2340efe079e371f
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-06-12 23:57:41 +0200

    feat(rustax/v0.2.0): add server init
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- rustax/v0.2/rustax/src/bin/server.rs
+++ rustax/v0.2/rustax/src/bin/server.rs
@@ -1,5 +1,30 @@
-use tax;
+#[macro_use] extern crate rocket;
 
-fn main() {
-    println!("Welcome to rustax server");
+use rocket::serde::{Serialize, json::Json};
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Amount {
+    amount: f32,
+}
+
+#[get("/urssaf?<amount>")]
+fn calc(amount: f32) -> Json<Amount> {
+    return Json(Amount { amount })
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/hello", routes![world])
+        .mount("/tax", routes![calc])
+        .launch()
+        .await?;
+
+    Ok(())
 }
