fb921b5e38403711d1cf9d1b1574a4104e1b9f7a
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-06-18 18:34:34 +0200

    feat(rustax/v2.0): finish server handler
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- rustax/v0.2/rustax/src/bin/server.rs
+++ rustax/v0.2/rustax/src/bin/server.rs
@@ -1,30 +1,9 @@
-#[macro_use] extern crate rocket;
+use server::server;
 
-use rocket::serde::{Serialize, json::Json};
-
-#[get("/world")]
-fn world() -> &'static str {
-    "Hello, world!"
-}
-
-#[derive(Serialize)]
-#[serde(crate = "rocket::serde")]
-struct Amount {
-    amount: f32,
-}
-
-#[get("/urssaf?<amount>")]
-fn calc(amount: f32) -> Json<Amount> {
-    return Json(Amount { amount })
-}
-
-#[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
-    let _rocket = rocket::build()
-        .mount("/hello", routes![world])
-        .mount("/tax", routes![calc])
-        .launch()
-        .await?;
-
-    Ok(())
+fn main() {
+    let foo = server();
+    match foo {
+        Ok(_) => {},
+        Err(err) => panic!("error {}", err),
+    };
 }
--- rustax/v0.2/server/src/lib.rs
+++ rustax/v0.2/server/src/lib.rs
@@ -1,8 +1,35 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::{json::Json, Serialize};
+use tax::Calculator;
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Amount {
+    amount: f32,
+}
+
+#[get("/?<amount>")]
+fn calc(amount: f32) -> Json<Amount> {
+    let calc = Calculator::new(amount);
+    let result = calc.taxe();
+
+    return Json(Amount { amount: result });
+}
+
+#[rocket::main]
+pub async fn server() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/hello", routes![world])
+        .mount("/tax", routes![calc])
+        .launch()
+        .await?;
+
+    Ok(())
 }
